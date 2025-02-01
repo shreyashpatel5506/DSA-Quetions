965b4c3787b5e68bd741ee29ff6c645d7d20bf61
Shreyash patel <shreyashpatel5506@gmail.com>
2025-02-02 00:44:11 +0530

    Add Java and Python implementations for finding the longest increasing subarray;

---
+++ longestsubarray/BYpython.py
@@ -0,0 +1,13 @@
+array = [2,2,2,2,2,2,2,2]
+count = 1
+max_length = 1
+for i in range(len(array)):
+    for j in range(i, len(array) - 1):
+        if array[j + 1] > array[j]:
+            count += 1
+            if count > max_length:
+                max_length = count
+        else:
+            break
+    count = 1
+print(max_length)

f7f5563e72d0f3d39677d4c302fc31c0d42d6f5b
Shreyash patel <shreyashpatel5506@gmail.com>
2025-02-02 00:18:35 +0530

    Enhance integer reversal implementations to handle negative numbers in Java and Python

--- reverseNumber/BYjava.java
+++ reverseNumber/BYjava.java
@@ -9,10 +9,13 @@ public class BYjava {
         System.out.println("Enter the number");
         int n = sc.nextInt();
         int rev = 0;
+        int sign = n < 0 ? -1 : 1;
+        n = Math.abs(n);
         while (n > 0) {
             rev = n % 10 + rev * 10;
             n /= 10;
         }
+        rev *= sign;
         System.out.println("Reverse of the number is " + rev);
         sc.close();
     }
--- reverseNumber/BYpython.py
+++ reverseNumber/BYpython.py
@@ -1,10 +1,15 @@
 number = int(input("Enter a number: "))
+sign = '-' if number < 0 else ''
+abs_number = abs(number)
 
-def reverse_number(number):
+def reverse_number(abs_number):
   reversed_number = 0
-  while number > 0:
-    reversed_number = reversed_number * 10 + (number % 10)
-    number = number // 10
-  print(reversed_number)
+  while abs_number > 0:
+    reversed_number = reversed_number * 10 + (abs_number % 10)
+    abs_number = abs_number // 10
+  return reversed_number
 
-reverse_number(number)
+reversed_number = reverse_number(abs_number)
+if sign:
+  reversed_number = int(sign + str(reversed_number))
+print(reversed_number)
